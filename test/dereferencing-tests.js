d9dce160b4ccae15cf26e0fc664ac3920a1eeed5
toby <tobytronics@gmail.com>
2015-08-26 14:58:36 +0100

    Dereference pointer to root document

---
+++ test/dereferencing-tests.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var jsonPointer = require('../lib');
+var expect = require('chai').expect;
+
+it('dereferences root document', function () {
+  var doc = {
+    name: {
+      firstName: 'Joe',
+      secondName: 'Smith'
+    }
+  },
+  pointer = '';
+
+  var result = jsonPointer.deref(pointer, doc);
+
+  expect(result).to.deep.equal(doc);
+});
--- test/sample-tests.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var expect = require('chai').expect;
-
-it('runs the tests', function () {
-  expect(true).to.equal(true);
-});
