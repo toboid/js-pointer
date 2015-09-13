4e863d939a561a10b249581a54543c825b27cb5e
toby <tobytronics@gmail.com>
2015-09-13 17:07:08 +0100

    Refactor to table-driven tests

--- test/dereferencing-tests.js
+++ test/dereferencing-tests.js
@@ -3,48 +3,33 @@
 const jsonPointer = require('../lib')
 const expect = require('chai').expect
 
-const testDoc = {
-  foo: ['bar', 'baz'],
-  person: { name: { firstName: 'bob' } },
-  '': 0,
-  'a/b': 1,
-  'c%d': 2,
-  'e^f': 3,
-  'g|h': 4,
-  'i\\j': 5,
-  'k\"l': 6,
-  ' ': 7,
-  'm~n': 8
-}
+describe('get()', function () {
+  const testDoc = {
+    foo: ['bar', 'baz'],
+    person: { name: { firstName: 'bob' } },
+    '': 0,
+    'a/b': 1,
+    'c%d': 2,
+    'e^f': 3,
+    'g|h': 4,
+    'i\\j': 5,
+    'k\"l': 6,
+    ' ': 7,
+    'm~n': 8
+  }
 
-it('dereferences root document', function () {
-  const pointer = ''
+  const tests = [
+    { pointer: '', expected: testDoc },
+    { pointer: '/foo', expected: ['bar', 'baz'] },
+    { pointer: 'person/name/firstName', expected: 'bob' },
+    { pointer: '/foo/0', expected: 'bar' }
+  ]
 
-  const result = jsonPointer.get(testDoc, pointer)
-
-  expect(result).to.deep.equal(testDoc)
+  tests.forEach(function (test) {
+    it('dereferences "' + test.pointer + '"', function () {
+      const result = jsonPointer.get(testDoc, test.pointer)
+      expect(result).to.deep.equal(test.expected)
+    })
+  })
 })
 
-it('dereferences first level node', function () {
-  const pointer = '/foo'
-
-  const result = jsonPointer.get(testDoc, pointer)
-
-  expect(result).to.deep.equal(['bar', 'baz'])
-})
-
-it('dereferences nested node', function () {
-  const pointer = 'person/name/firstName'
-
-  const result = jsonPointer.get(testDoc, pointer)
-
-  expect(result).to.deep.equal('bob')
-})
-
-it('dereferences array elements', function () {
-  const pointer = '/foo/0'
-
-  const result = jsonPointer.get(testDoc, pointer)
-
-  expect(result).to.be.deep.equal('bar')
-})
