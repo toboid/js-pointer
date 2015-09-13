7b83709808951ed4326d9cbc53f4a83c541da32a
toby <tobytronics@gmail.com>
2015-09-13 15:51:43 +0100

    Fixes for standard.js style

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-'use strict';
-
 // TODO rename deref to get
 module.exports = {
   deref: deref
-};
+}
 
 // TODO put the doc first
 function deref (pointer, doc) {
-  const segments = compact(pointer.split('/'));
-  return traverse(doc, segments);
+  const segments = compact(pointer.split('/'))
+  return traverse(doc, segments)
 }
 
 function traverse (doc, segments) {
-  if(!segments.length) return doc;
+  if (!segments.length) return doc
 
-  return traverse(doc[first(segments)], segments.slice(1));
+  return traverse(doc[first(segments)], segments.slice(1))
 }
 
 function compact (collection) {
-  return collection.filter((item) => item);
+  return collection.filter((item) => item)
 }
 
 function first (arr) {
-  return arr[0];
+  return arr[0]
 }
