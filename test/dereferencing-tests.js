23bfd0855ab4a158e06f21e945d8137e5b5383ce
toby <tobytronics@gmail.com>
2015-09-13 19:00:22 +0100

    Keep spec examples seperate

--- test/dereferencing-tests.js
+++ test/dereferencing-tests.js
@@ -4,31 +4,40 @@ const jsonPointer = require('../lib')
 const expect = require('chai').expect
 
 describe('get()', function () {
-  const testDoc = {
-    foo: ['bar', 'baz'],
-    person: { name: { firstName: 'bob' } },
-    '': 0,
-    'a/b': 1,
-    'c%d': 2,
-    'e^f': 3,
-    'g|h': 4,
-    'i\\j': 5,
-    'k\"l': 6,
-    ' ': 7,
-    'm~n': 8
-  }
+  it('dereferences arbitrary length pointer', function () {
+    const testDoc = { one: { two: { three: [{ four: 4 }] } } };
+    const result = jsonPointer.get(testDoc, '/one/two/three/0/four')
+    expect(result).to.deep.equal(4)
+  })
 
-  const tests = [
-    { pointer: '', expected: testDoc },
-    { pointer: '/foo', expected: ['bar', 'baz'] },
-    { pointer: 'person/name/firstName', expected: 'bob' },
-    { pointer: '/foo/0', expected: 'bar' }
-  ]
+  describe('rfc6901 compliance', function () {
+    // See https://tools.ietf.org/html/rfc6901#section-5
 
-  tests.forEach(function (test) {
-    it('dereferences "' + test.pointer + '"', function () {
-      const result = jsonPointer.get(testDoc, test.pointer)
-      expect(result).to.deep.equal(test.expected)
+    const testDoc = {
+      foo: ['bar', 'baz'],
+      '': 0,
+      'a/b': 1,
+      'c%d': 2,
+      'e^f': 3,
+      'g|h': 4,
+      'i\\j': 5,
+      'k\"l': 6,
+      ' ': 7,
+      'm~n': 8
+    }
+
+    const tests = [
+      { pointer: '', expected: testDoc },
+      { pointer: '/foo', expected: ['bar', 'baz'] },
+      { pointer: '/foo/0', expected: 'bar' },
+      { pointer: '/', expected: 0 }
+    ]
+
+    tests.forEach(function (test) {
+      it('dereferences "' + test.pointer + '"', function () {
+        const result = jsonPointer.get(testDoc, test.pointer)
+        expect(result).to.deep.equal(test.expected)
+      })
     })
   })
 })
