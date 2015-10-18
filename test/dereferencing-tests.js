6a57ac61fe590a21e1d8d8f4ff9b4803a6b77709
toboid <toboid@users.noreply.github.com>
2015-10-18 11:48:48 +0100

    Babelify tests

--- test/dereferencing-tests.js
+++ test/dereferencing-tests.js
@@ -1,24 +1,24 @@
 'use strict'
 
-var jsonPointer = require('../dist')
-var expect = require('chai').expect
+const jsonPointer = require('../lib')
+const expect = require('chai').expect
 
 describe('get()', function () {
   it('dereferences arbitrary length pointer', function () {
-    var testDoc = { one: { two: { three: [{ four: 4 }] } } }
-    var result = jsonPointer.get(testDoc, '/one/two/three/0/four')
+    const testDoc = { one: { two: { three: [{ four: 4 }] } } }
+    const result = jsonPointer.get(testDoc, '/one/two/three/0/four')
     expect(result).to.deep.equal(4)
   })
 
   it('returns undefined when pointer references non-existent value', function () {
-    var testDoc = { one: 1 }
-    var result = jsonPointer.get(testDoc, '/two')
+    const testDoc = { one: 1 }
+    const result = jsonPointer.get(testDoc, '/two')
     expect(result).to.be.undefined
   })
 
   describe('validation', function () {
     it('throws for pointers not beginning with "/"', function () {
-      var expectedErrorMatcher = /Non-empty pointer must start with "\/"/
+      const expectedErrorMatcher = /Non-empty pointer must start with "\/"/
       expect(function () {
         jsonPointer.get({}, 'one')
       }).to.throw(Error, expectedErrorMatcher)
@@ -28,7 +28,7 @@ describe('get()', function () {
   describe('rfc6901 compliance', function () {
     // See https://tools.ietf.org/html/rfc6901#section-5
 
-    var testDoc = {
+    const testDoc = {
       foo: ['bar', 'baz'],
       '': 0,
       'a/b': 1,
@@ -41,7 +41,7 @@ describe('get()', function () {
       'm~n': 8
     }
 
-    var tests = [
+    const tests = [
       { pointer: '', expected: testDoc },
       { pointer: '/foo', expected: ['bar', 'baz'] },
       { pointer: '/foo/0', expected: 'bar' },
@@ -58,7 +58,7 @@ describe('get()', function () {
 
     tests.forEach(function (test) {
       it('dereferences "' + test.pointer + '"', function () {
-        var result = jsonPointer.get(testDoc, test.pointer)
+        const result = jsonPointer.get(testDoc, test.pointer)
         expect(result).to.deep.equal(test.expected)
       })
     })
